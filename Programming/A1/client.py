65b5d2c0b017bada846b1ba864d69b263194058a
TWijesundera <thisarawijesundera@gmail.com>
2020-01-29 15:05:08 -0500

    Made changes to use python3. Additionally, fixed character count calculation

--- Programming/A1/client.py
+++ Programming/A1/client.py
@@ -1,14 +1,16 @@
+# Thisara Wijesundera
+# CS 351
+# Assignment 1: client.py
+
 """
-    Thisara Wijesundera
-    CS 351
-    Assignment 1: client.py
+    MUST BE RUN USING PYTHON3
 
     Description:
         This program is meant to recieve number or characters,
         words, and lines in the file sent to the server
 
     Usage:
-        python client.py <server_ip_address> <port_number> <path_to_file>
+        python3 client.py <server_ip_address> <port_number> <path_to_file>
         
         <path_to_file> should be a text file???
 
@@ -16,49 +18,69 @@
         hostname, port
         filename = {} number of characters, {} number of words, {} number of lines
 """
+
 import socket
 import sys
+import os
 
-def sanitizeIP(ip_address):
-	return ip_address.count('.') == 3
+def sanitize_ip(ip_address: str) -> bool:
+    """ Return bool if ip_address has 3 periods """
+    return ip_address.count('.') == 3
 
-def sanitizePort(port_number):
-	return len(port_number) > 3
+def sanitize_port(port_number: str) -> bool:
+    """ Return bool if port number is less than 3 numbers """
+    return len(port_number) > 3
+
+def check_file_size(file_path: str) -> bool:
+    """ Return bool if file size is greater than 4096 bytes """
+    return os.stat(file_path).st_size > 4096
 
 
 if __name__  == "__main__":
     try:
         if len(sys.argv) != 4:
             raise Exception("\nMake sure you are running the program correctly " \
-            "python client.py <server_ip> <port_number> <file_path>\n")
+            "python3 client.py <server_ip> <port_number> <file_path>\n")
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Get local machine name
-        if sanitizeIP(sys.argv[1]):
+        if sanitize_ip(sys.argv[1]):
             host = sys.argv[1]
         else:
             raise Exception("\nPlease make sure the IP address is correct\n")
         
-        if sanitizePort(sys.argv[2]):
+        if sanitize_port(sys.argv[2]):
             port = int(sys.argv[2])
         else:
             raise Exception("\nPlease choose a higher port number\n")
+        
+        if not check_file_size(sys.argv[3]):
+            f = sys.argv[3]
+        else:
+            raise Exception("\nFile size is too large. Please pick a different file\n")
 
         s.connect((host, port))
+        
+        file_to_send = open(f, 'rb')
+        transmit = file_to_send.read(4096)
+
+        """
+            While there is data in the file
+            transmit the data and read again
+        """
+        while transmit:
+            s.sendall(transmit)
+            transmit = file_to_send.read(4096)
+
+        s.shutdown(socket.SHUT_WR)
+
+        # Recieve no more than 4096 bytes
+        msg = s.recv(4096)
+        s.close()
+        print(msg.decode('ascii'))
+
     except Exception as e:
         print(e)
 
-    file_to_send = open(sys.argv[3], 'rb')
-    transmit = file_to_send.read(4096)
 
-    while transmit:
-        s.sendall(transmit)
-        transmit = file_to_send.read(4096)
-
-    s.shutdown(socket.SHUT_WR)
-
-    # Recieve no more than 4096 bytes
-    msg = s.recv(1024)
-    s.close()
-    print(msg.decode('ascii'))
--- Programming/A1/server.py
+++ Programming/A1/server.py
@@ -1,8 +1,8 @@
-"""
-    Server program to run on EC2
-    CS 351
-    Thisara Wijesundera
+# Server program to run on EC2
+# CS 351
+# Thisara Wijesundera
 
+"""
     Description:
         This program is meant to recieve a file from the client
         and process the file
@@ -15,34 +15,62 @@
     Returns:
         Sends number of characters, words, and lines to the client
 """
+
 import socket
 import sys
 
-def sanitizePort(port_number):
-	return len(port_number) > 3
 
-def processFile ():
+def sanitizePort(port_number: str) -> bool:
+    """ Return bool if port number is less than 3 numbers """
+    return len(port_number) > 3
+
+def processFile() -> list:
+    """Counts the number of lines, words, and charcters in a file
+
+        Args:
+            No value
+        
+        Returns:
+            list[int]: [number of lines, number of words, number of characters]
+                        contained in 'countfile'
+        
+        Todo:
+            Instead use the recieved buffer to modify without creating/reading
+                from count file
+        
+    """
     num_chars = 0
     num_words = 0
 
-    with open("./countfile", 'r') as file:
+    with open("./countfile", 'rb') as f:
+        """Count the number of words, lines, and charaters for every line in file
+            
+            Note:
+                'line_num' is 0 based (becuase of enumerate), therefore must add 1 when returning
+                Adding the count of spaces and '\n' characters which are stripped during 'line.split'
         """
-            Write comment for loop here
-        """
-        for line_num, line in enumerate(file):
-            words = line.split()
-            num_words += len(words)
-            num_chars += sum(len(word) for word in words)
 
-    # Must add the length of "words" because they were split by spaces
-    return [line_num+1, num_words, num_chars+len(words)]
+
+        for line_num, line in enumerate(f):
+            words = line.split() # Words in line (List[str])
+            # print(words)
+            num_words += len(words)
+            # Generator function which calculates the sum of all words in a line
+            # print([word for word in words])
+            num_chars += sum(len(word) for word in words) + line.count(b' ')+ line.count(b'\n')
+    
+    return [line_num+1, num_words, num_chars]
+    
     
 if __name__ == "__main__":
-    # Create a socket object
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # Get local machine name
     try:
+        if len(sys.argv) != 2:
+            raise Exception("\nMake sure you are running the program correctly. " \
+            "python3 server.py <port_number>\n")
+    
+    # Create a socket object
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         host = socket.gethostname()
         if sanitizePort(sys.argv[1]):
             port = int(sys.argv[1])
@@ -60,7 +88,7 @@ if __name__ == "__main__":
 
     try:
         """
-            Write comment for loop
+            While True to keep server available to take connections from clients
         """
         while True:
             infile = open('./countfile', 'wb')
@@ -72,17 +100,20 @@ if __name__ == "__main__":
                 infile.write(rec)
                 rec = client_sock.recv(4096)
 
+            infile.close()
+
             print("Got a connection from {}".format(str(addr)))
 
             counts = processFile()
-            print(counts)
+            # print(counts)
 
             msg = "Hostname: {} \n Port: {} \n Number of lines: {} \
-            \n Number of words: {} \n Number of characters: {} \r\n".format(host, port, counts[0], counts[1], counts[2])
+                    \n Number of words: {} \n Number of characters: {} \r\n" \
+                    .format(host, port, counts[0], counts[1], counts[2])
+
             client_sock.sendall(msg.encode('ascii'))
             client_sock.close()
 
     except KeyboardInterrupt:
         print("\n")
         server_sock.close()
-
