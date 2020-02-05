3ad647ca7c4343d3b9849360eef014e266f36601
TWijesundera <thisarawijesundera@gmail.com>
2020-02-04 21:07:09 -0500

    not sure what happend

---
+++ Programming/A2/rpc/multiplyServer.py
@@ -0,0 +1,42 @@
+"""RPC Server that multiplies 2 numbers and send answer to client"""
+# Thisara Wijesundera
+# CS 351
+# RPC Assignment 2
+
+import sys
+
+from xmlrpc.server import SimpleXMLRPCServer
+
+def mutiply_nums(num_1: int, num_2: int) -> int:
+    """Mutiplies 2 numbers
+
+        Args:
+            num_1 (int): number given to the server
+            num_2 (int): number given to the server
+
+        Returns:
+            Two given numbers multiplied together
+    """
+    return num_1 * num_2
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please provide all required arguements\n")
+        sys.exit()
+    else:
+        try:
+            SERVER = SimpleXMLRPCServer(("0.0.0.0", int(sys.argv[1])))
+            SERVER.register_introspection_functions()
+            SERVER.register_function(mutiply_nums, "mul")
+
+            print("Press CTRL+C to stop server\n")
+            SERVER.serve_forever()
+
+        except ValueError as err:
+            print("Incorrect port number\nERROR: {}\n".format(err))
+
+        except PermissionError as err:
+            print("Port in use\nERROR: {}\n".format(err))
+
+        except KeyboardInterrupt:
+            print('\n')
