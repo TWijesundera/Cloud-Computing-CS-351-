4cd05aacfecaca343b13bc55b7b71ef8d3d4b0f5
TWijesundera <thisarawijesundera@gmail.com>
2020-02-23 11:52:49 -0500

    Modified the server to add 'X' or 'O' to person value dict Works with dictionary and can send msgs between peers

--- Programming/A3/game/Server.py
+++ Programming/A3/game/Server.py
@@ -2,106 +2,136 @@ import sys
 import socket
 import select
 
-from .Board import Board
+from Board import Board
 from typing import Dict, List
 
-HOST = '0.0.0.0' 
-SOCKET_LIST_X = []
-SOCKET_LIST_Y = []
 count = 0
-RECV_BUFFER = 4096 
-PORT = 9009
+RECV_BUFFER = 4096
 
 class Server:
     
-    def __init__():
+    symbol = 'X'
+
+    def __init__(self, HOST, PORT):
         """Initilize server with some variables
 
             Variables:
-                x_players (Dict): Key - player namea  Value - 'X'
-                o_players (Dict): Key - player name   Value - 'O'
+                player_sockets (Dict): key - address       values - tuple(socket object, 'x' or 'o')
                 board (Board): Initalize the Tic Tac Toe board object
         """
-        self.board = Board()
+        self.game_board = Board()
+        self.players_sockets = {}
+        try:
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.server_socket.bind((HOST, PORT))
+            self.server_socket.listen(10)
+            print(f"Chat server started on port {str(PORT)}\n")
 
-    def start_server():
-        """Method that starts the tcp dserver"""
+            # add server socket object to the list of readable connections
+            self.players_sockets[self.server_socket.getsockname()] = (self.server_socket, 'server')
+        except socket.error as msg:
+            print(f"ERROR {msg}")
+            sys.exit()
+
+    def broadcast_board(self):
         pass
 
-    def broadcast_board():
-        pass
-
-def chat_server():
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(10)
- 
-    # add server socket object to the list of readable connections
-    SOCKET_LIST_X.append(server_socket)
- 
-    print (f"Chat server started on port {str(PORT)}")
- 
-    while True:
-        # get the list sockets which are ready to be read through select
-        # 4th arg, time_out  = 0 : poll and never block
-        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST_X,[],[],0)
-      
-        for sock in ready_to_read:
-            # a new connection request recieved
-            if sock == server_socket: 
-                sockfd, addr = server_socket.accept()
-                SOCKET_LIST_X.append(sockfd)
-                print ("Client (%s, %s) connected" % addr)
-                 
-                broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
-             
-            # a message from a client, not a new connection
-            else:
-                # process data recieved from client, 
-                try:
-                    # receiving data from the socket.
-                    data = sock.recv(RECV_BUFFER).decode('UTF-8')
-                    if data:
-                        # there is something in the socket
-                        broadcast(server_socket, sock, f"\r[{str(sock.getpeername())}] {data}")  
+    def chat_server(self):
+        while True:
+            server_socket = self.server_socket
+            try:
+                # get the list sockets which are ready to be read through select
+                # 4th arg, time_out  = 0 : poll and never block
+                ready_to_read,ready_to_write,in_error = select.select([x[0] for x in self.players_sockets.values()],[],[],0)
+            
+                for sock in ready_to_read:
+                    # print("ready list: {} ".format(ready_to_read))
+                    # print("ready sock {} ".format(sock))
+                    # a new connection request recieved
+                    if sock == server_socket: 
+                        sockfd, addr = server_socket.accept()
+                        self.players_sockets[addr] = (sockfd, self.assign_player())
+                        # print(f"\nsocket list: {self.players_sockets}\n")
+                        print ("Client (%s, %s) connected" % addr)
+                        
+                        self.broadcast(sockfd, "[%s:%s] entered the chat room\n" % addr)
+                    
+                    # a message from a client, not a new connection
                     else:
-                        # remove the socket that's broken    
-                        if sock in SOCKET_LIST_X:
-                            SOCKET_LIST_X.remove(sock)
+                        # process data recieved from client, 
+                        try:
+                            # receiving data from the socket.
+                            data = sock.recv(RECV_BUFFER).decode('UTF-8')
+                            # print(f"after data recv else statement: {data}")
+                            if data:
+                                # there is something in the socket
+                                # print(f"recieved data: {data}")
+                                self.broadcast(sock, f"\r[{str(sock.getpeername())}] {data}")  
+                            else:
+                                # remove the socket that's broken    
+                                if sock in self.players_sockets:
+                                    # print("Sock in else: {}".format(sock))
+                                    self.players_sockets.pop(sock.getpeername())
 
-                        # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+                                # at this stage, no data means probably the connection has been broken
+                                self.broadcast(sock, "Client (%s, %s) is offline\n" % addr) 
 
-                # exception 
-                except:
-                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
-                    continue
-
-    server_socket.close()
+                        # exception 
+                        except Exception as e:
+                            # print(f"execption offline : {e}\n")
+                            self.broadcast(sock, "Client (%s, %s) is offline\n" % addr)
+                            continue
+            except KeyboardInterrupt:
+                return
     
-# broadcast chat messages to all connected clients
-def broadcast (server_socket, sock, message):
-#whoTo = 0 send only x players
-#whoTO = 1 send to only Y
-#whoTo = 2 send to all
-    for socket in SOCKET_LIST_X:
-        # send the message only to peer
-        if socket != server_socket and socket != sock :
-            try :
-                socket.send(bytes(message, 'UTF-8'))
-            except :
-                # broken socket connection
-                socket.close()
-                # broken socket, remove it
-                if socket in SOCKET_LIST:
-                    SOCKET_LIST.remove(socket)
+    # broadcast chat messages to all connected clients
+    def broadcast (self, sock, message):
+    #whoTo = 0 send only x player
+    #whoTO = 1 send to only Y
+    #whoTo = 2 send to all
+        print(f"sockfd: {sock}\n")
+        for socket in self.players_sockets:
+            # send the message only to peer
+            print(f"if {socket} != {self.server_socket.getsockname()} and {socket} != {sock.getpeername()}")
+            if socket != self.server_socket.getsockname() and socket != sock.getpeername():
+                try :
+                    # print(f"\"{message}\" sent to: {socket}")
+                    self.players_sockets[socket][0].send(bytes(message, 'UTF-8'))
+                except Exception as e:
+                    # broken socket connection
+                    self.players_sockets[socket][0].close()
+                    # broken socket, remove it
+                    if socket in self.players_sockets:
+                        self.players_sockets.pop(socket)
+                    print("\nERROR: {e}\n")
 
-def assign_player():
-    """ Assigns the player to X or O
-    """
+    def assign_player(self):
+        """ Assigns the player to X or O
+        """
+        symbol = Server.symbol
+
+        if Server.symbol == 'X':
+            Server.symbol = 'O'
+        else:
+            Server.symbol = 'X'
+        
+        return symbol
  
 if __name__ == "__main__":
+    HOST = '0.0.0.0'
+    PORT = 9009
+    server = Server(HOST, PORT)
+    try:
+        print(server.game_board.__str__())
+        server.chat_server()
+    
+    except KeyboardInterrupt:
+        server.server_socket.close()
+        sys.exit()
 
-    sys.exit(chat_server())
+"""
+    except Exception as e:
+        print(f"ERROR: {e}")
+        sys.exit()
+        """
